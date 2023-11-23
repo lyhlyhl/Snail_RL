f044c110ca8bbf8230b96ce47b6f377eb11ff262
BIMK-PC-LYH\11799 <1179969765@qq.com>
2023-11-23 13:19:00 +0800

    写了一坨屎

--- assist/Flappy bird/FlappyBird.py
+++ assist/Flappy bird/FlappyBird.py
@@ -1,6 +1,5 @@
 import pygame
 import random
-
 # 初始化pygame
 pygame.init()
 
@@ -13,8 +12,9 @@ FPS = 30
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 
+score = 0
 # 定义小鸟类
-class Bird(pygame.sprite.Sprite):
+class Bird():
     def __init__(self):
         super().__init__()
         # 加载小鸟图像
@@ -48,7 +48,7 @@ class PipePair(pygame.sprite.Sprite):
         self.bottomMove = random.randint(0,50)
 
 
-    def update(self):
+    def update(self,bird):
         self.x -= 5
         self.rect_top.topleft = (self.x, 0 - self.topMove)
         self.rect_bottom.topleft = (self.x, 300 - self.topMove + self.gap)
@@ -59,50 +59,57 @@ class PipePair(pygame.sprite.Sprite):
             global score
             score += 1
 
-# 初始化小鸟和柱子组
-bird = Bird()
-pipes = pygame.sprite.Group()
-old_time = pygame.time.get_ticks()
-background_image = pygame.image.load('background.png')
-reward = 0
-score = 0
-# 游戏循环
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                bird.jump()
-    current_time = pygame.time.get_ticks()
-    # 添加柱子对
-    if current_time - old_time > 1500:  # 控制屏幕上同时出现的柱子对数量
-        new_pipe = PipePair(SCREEN_WIDTH)
-        pipes.add(new_pipe)
-        old_time = current_time
+class Mygame():
+    def __init__(self):
+        self.background_image = pygame.image.load('background.png')
+    def reset(self):
+        self.bird = Bird()
+        self.pipes = pygame.sprite.Group()
+        self.old_time = pygame.time.get_ticks()
+        self.reward = 0
+        score = 0
+    def render(self):
+        pass
 
-    bird.update()
-    pipes.update()
+    def run(self):
+        for event in pygame.event.get(): # 鸟的动作
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.bird.jump()
+        current_time = pygame.time.get_ticks()
+        # 添加柱子对
+        if current_time - self.old_time > 1500:  # 控制屏幕上同时出现的柱子对数量
+            new_pipe = PipePair(SCREEN_WIDTH)
+            self.pipes.add(new_pipe)
+            self.old_time = current_time
 
-    # 绘制背景、小鸟和柱子
-    screen.blit(background_image, (0, 0))
-    screen.blit(bird.image, bird.rect)
+        self.bird.update()
+        self.pipes.update(self.bird)
 
-    for pipe_pair in pipes:
-        screen.blit(pipe_pair.image_top, (pipe_pair.x, 0-pipe_pair.topMove))
-        screen.blit(pipe_pair.image_bottom, (pipe_pair.x, 300 - pipe_pair.topMove + pipe_pair.gap))
-        if bird.rect.colliderect(pipe_pair.rect_top) or bird.rect.colliderect(pipe_pair.rect_bottom):
-            running = False
-            print("over, Score = {}".format(score))
+        # 绘制背景、小鸟和柱子
+        screen.blit(self.background_image, (0, 0))
+        screen.blit(self.bird.image, self.bird.rect)
 
-    font = pygame.font.Font(None, 36)  # 设置字体和字号
-    text = font.render(f'Score: {score}', True, (0, 0, 0))  # 渲染得分文本
-    screen.blit(text, (10, 10))  # 显示得分文本的位置
+        for pipe_pair in self.pipes:
+            screen.blit(pipe_pair.image_top, (pipe_pair.x, 0 - pipe_pair.topMove))
+            screen.blit(pipe_pair.image_bottom, (pipe_pair.x, 300 - pipe_pair.topMove + pipe_pair.gap))
+            if self.bird.rect.colliderect(pipe_pair.rect_top) or self.bird.rect.colliderect(pipe_pair.rect_bottom):
+                return (0, score)
+        #todo:1.游戏的环境应该怎么写
+        #   2.游戏的图像获取作为神经网络的输入
+        #   3.如何又训练神经网络又训练
+        font = pygame.font.Font(None, 36)  # 设置字体和字号
+        text = font.render(f'Score: {score}', True, (0, 0, 0))  # 渲染得分文本
+        screen.blit(text, (10, 10))  # 显示得分文本的位置
 
-    # 游戏逻辑更新  
+        # 游戏逻辑更新
 
+        pygame.display.flip()
+        clock.tick(FPS)
+        return (1, )
 
-    pygame.display.flip()
-    clock.tick(FPS)
-pygame.quit()
+gameEnv = Mygame()
+gameEnv.reset()
+gameEnv.run()
