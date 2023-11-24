b48751696ffad0f8de848c266f067dcea5358822
BIMK-PC-LYH\11799 <1179969765@qq.com>
2023-11-24 16:25:22 +0800

    改良了一些bug 跑得好慢，换个电脑试试

--- assist/Flappy bird/DQN.py
+++ assist/Flappy bird/DQN.py
@@ -9,18 +9,30 @@ from torchvision import models
 class DQN(nn.Module):
     def __init__(self, input_channels, num_actions):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1)
-        self.fc1_input_size = 64 * 7 * 27 * 64 * 5 * 25
-        self.fc1 = nn.Linear(self.fc1_input_size, 128)  #
-        self.fc2 = nn.Linear(128, num_actions)
+        self.conv1 = nn.Conv2d(in_channels=input_channels, out_channels=16, kernel_size=4, stride=4)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, stride=1)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 重新计算全连接层的输入大小
+        conv1_output_size = (16, 128, 72)
+        conv2_output_size = (32, 32, 36)
+        conv3_output_size = (16, 15, 17)
+        fc1_input_size = 288
+
+        self.fc1 = nn.Linear(fc1_input_size, 64)
+        self.fc2 = nn.Linear(64, num_actions)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
+        x = self.pool1(x)
         x = F.relu(self.conv2(x))
+        x = self.pool2(x)
         x = F.relu(self.conv3(x))
-        x = x.view(-1, self.fc1_input_size)  #
+        x = self.pool3(x)
+        x = x.reshape(-1, 288)
         x = F.relu(self.fc1(x))
         q_values = self.fc2(x)
         return q_values
@@ -36,15 +48,15 @@ class ReplayBuffer:
         #state图像数组 action 1/0 reward 1 = -1 0 = 0 fail = -100 pass = 10
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
-        self.buffer[self.position] = (state, action, reward, next_state, done)
+        self.buffer[self.position] = (np.transpose(state, (2, 0, 1)), action, reward, np.transpose(next_state, (2, 0, 1)), done)
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
         return zip(*random.sample(self.buffer, batch_size))
 
 # 定义DQN Agent
-class DQNAgent:
-    def __init__(self, state_size, action_size, gamma=0.99, epsilon=0.1, lr=0.001, buffer_capacity=10000, batch_size=64):
+class DQNAgent():
+    def __init__(self, state_size, action_size, gamma=0.99, epsilon=0.1, lr=0.001, buffer_capacity=1000, batch_size=16):
         self.state_size = state_size
         self.action_size = action_size
         self.gamma = gamma
@@ -74,8 +86,9 @@ class DQNAgent:
             return
 
         states, actions, rewards, next_states, dones = self.buffer.sample(self.batch_size)
-
         states = torch.FloatTensor(states)
+        print(states.shape)
+
         actions = torch.LongTensor(actions)
         rewards = torch.FloatTensor(rewards)
         next_states = torch.FloatTensor(next_states)
